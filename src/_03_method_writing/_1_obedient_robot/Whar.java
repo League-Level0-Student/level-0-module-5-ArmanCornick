1fbf79d0623bce86d550599d44a441c7d907d9c1
league <league@wts14-3.attlocal.net>
2023-09-16 12:58:51 -0700

    adsdasw

--- src/_03_method_writing/_1_obedient_robot/Whar.java
+++ src/_03_method_writing/_1_obedient_robot/Whar.java
@@ -2,18 +2,29 @@ package _03_method_writing._1_obedient_robot;
 
 import java.awt.Color;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Whar {
 public static void main(String[] args) {	
 Robot rob = new Robot();	
 rob.setSpeed(130123012);
+String color = JOptionPane.showInputDialog("shape?");
+String shape = JOptionPane.showInputDialog("color for shape (red blue green and red)");
+if(shape.equals("red")) {
+if((color.equals("square"))) {
 drawSquare(Color.red,rob);	
+}
+if(color.equals("traingle")) {
+drawTriangle(Color.red,rob);
+}
+}
+
 	
 	
 	
-	
-	
+				
 	
 	
 	
@@ -32,6 +43,14 @@ static void drawSquare(Color color, Robot r) {
 	for(int i=0;i<4;i++) {
 	r.move(50);
 	r.turn(90);
+	}
+}
+static void drawTriangle(Color color, Robot r) {	
+	r.penDown();
+	r.setPenColor(color);
+	for(int i=0;i<3;i++) {
+	r.move(100);
+	r.turn(120);
 	}
 }
 }
